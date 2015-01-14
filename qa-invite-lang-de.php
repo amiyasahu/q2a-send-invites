f2516dfe340d463223eb4c033169bd9f8ad93d0f
Amiya Sahu <amiya053@gmail.com>
2015-01-15 01:22:51 +0530

    added german lang support

---
+++ qa-invite-lang-de.php
@@ -0,0 +1,53 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+	QA Invite Plugin (c) Amiya Sahu (developer.amiya@outlook.com)
+	German Language by Martin Staffhorst
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/qa-invite/qa-invite-lang-default.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: US English language phrases for invite plugin
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'invite'	           => 'Empfehlen Sie ^site_title. Laden Sie andere Nutzer ein.',
+        'page_title'           => 'Einladung',
+        'invite_succ'          => 'Ihre Einladung(en) wurde(n) erfolgreich versendet',
+        'invite_failed'        => 'Ihre Einladung(en) wurde(n) nicht versendet',
+        'sender_name_lable'    => 'Name des Absenders *',
+        'sender_name_plc'      => 'Ihr Name',
+        'send_invites'         => 'Einladungen versenden',
+        'subject_lable'        => 'Betreff *',
+        'subject_plc'          => 'Betreff der Nachricht',
+        'recipient_addr_lable' => 'Empfängeradresse(n) *',
+        'recipient_addr_plc'   => 'Empfängeradresse(n)',
+        'recipient_addr_note'  => 'Falls Sie mehrere Personen auf einmal einladen möchten, trennen Sie die Email-Adressen bitte durch Kommas.',
+        'required_field'       => 'Dies ist ein Pflichtfeld',
+        'subject'              => 'Einladung zur Plattform ^site_title',
+        'message_lable'        => 'Nachricht',
+        'message_plc'          => 'Text der Nachricht',                       
+        'message'              => 'Guten Tag, ich würde Sie gerne zur Plattform ^site_title einladen.   Mit freundlichen Grüßen ',
+        'thanks'               => "\n\n\nGesendet von <a href=\"^user_url\">^sender_name</a> über <a href=\"^site_url\">^site_title</a> ",
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
